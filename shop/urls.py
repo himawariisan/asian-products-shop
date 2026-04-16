f82ca8b31d6489a76663d44eab4b27abd2cee144
Sofiia <sofarukomeda@gmail.com>
2026-04-16 22:36:08 +0300

    feat: design other pages added raw

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [    
     path('', views.index, name='index'),
     path('shop-details/', views.shop_details, name='shop_details'),
+    path('shop-grid/', views.index, name='shop_grid'),
     path('shopping-cart/', views.shopping_cart, name='shopping_cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('blog-details/', views.blog_details, name='blog_details'),
