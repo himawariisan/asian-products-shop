cf1bb01033e7f14aaed6ce76d73f750fca8f7ff6
Sofiia <sofarukomeda@gmail.com>
2026-04-16 22:07:17 +0300

    feat: Design template integrated main page

---
+++ shop/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [    
+    path('', views.index, name='index'),
+    path('shop-details/', views.shop_details, name='shop_details'),
+    path('shopping-cart/', views.shopping_cart, name='shopping_cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('blog-details/', views.blog_details, name='blog_details'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name='contact'),
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'shop/index.html')
+
+def blog_details(request):
+    return render(request, 'shop/blog-details.html')
+
+def blog(request):
+    return render(request, 'shop/blog.html')
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')
+
+def contact(request):
+    return render(request, 'shop/contact.html')
+
+def shopping_cart(request):
+    return render(request, 'shop/shopping-cart.html')
+
+def shop_details(request):
+    return render(request, 'shop/shop-details.html')
