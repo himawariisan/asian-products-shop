9bccdcbc5e0bc07e2c3b10be784b292ac6bbd179
Anastasiia Pudova <anastasiia.pudova@gmail.com>
2026-04-25 20:56:35 +0300

    feat: set default auto field for models

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
