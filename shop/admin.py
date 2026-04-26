8af44467120ec51a5b6b2108a9423e31780774e2
Sofiia <sofarukomeda@gmail.com>
2026-04-27 00:53:03 +0300

    feat: Configure admin panel and link existing products to categories

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from .models import Product
+from .models import Category, Product, Cart, CartItem
 
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price']
+    list_filter = ['category']
+    list_editable = ['price']
+    fields = ['category', 'name', 'description', 'price', 'image']
+
+
+admin.site.register(Cart)
+admin.site.register(CartItem)
