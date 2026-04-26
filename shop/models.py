6a7566a38544dc9f0c546ba19d86a77e242dc951
Sofiia <sofarukomeda@gmail.com>
2026-04-27 00:27:57 +0300

    feat: Add Category model and link it to Product

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', null=True, blank=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -39,3 +50,4 @@ class CartItem(models.Model):
     
     def get_subtotal(self):
         return self.product.price * self.quantity
+
