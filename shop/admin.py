5812d37b7347c2f5ff9ffc4ce2e2fa9a3bbaf015
Anastasiia Pudova <anastasiia.pudova@gmail.com>
2026-04-27 15:03:59 +0300

    fix

--- shop/admin.py
+++ shop/admin.py
@@ -13,13 +13,6 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['price']
     fields = ['category', 'name', 'description', 'price', 'image']
 
-
 admin.site.register(Cart)
 admin.site.register(CartItem)
-from .models import Product, Cart, CartItem, Order, OrderItem
 
-admin.site.register(Product)
-admin.site.register(Cart)
-admin.site.register(CartItem)
-admin.site.register(Order)
-admin.site.register(OrderItem)
