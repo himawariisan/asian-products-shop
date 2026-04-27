d5a942ad8c13fbdb38e315148db4cc558e2a6f8a
Anastasiia Pudova <anastasiia.pudova@gmail.com>
2026-04-27 11:56:22 +0300

    feat: add OrderItem model

--- shop/models.py
+++ shop/models.py
@@ -46,9 +46,57 @@ class Order(models.Model):
     STATUS_CHOICES = [
         ('pending', 'Очікує обробки'),
         ('processing', 'В обробці'),
-        ('shippeed', 'Відправлено'),
+        ('shipped', 'Відправлено'),
         ('delivered', 'Доставлено'),
         ('cancelled', 'Скасовано')
     ]
 
-    user = models.ForeignKey
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL,
+        null=True, 
+        blank=True, 
+        related_name='orders'
+    )
+
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='pending'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order #{self.pk} - {self.name}"
+    
+    def get_total(self):
+        return sum(item.get_subtotal() for item in self.order_items.all())
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, 
+        on_delete=models.CASCADE,
+        related_name='order_items'
+    )
+
+
+    product = models.ForeignKey(
+            Product, 
+            on_delete=models.SET_NULL, 
+            null=True, 
+            related_name='order_items'
+        )
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity}x {self.product.name if self.product else 'Видалений товар'}"
+        
+    def get_subtotal(self):
+        return self.price * self.quantity
